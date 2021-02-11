c69bf767938fb442a7d8d298433e29d47c6a91d2
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-11 15:56:12 -0300

    estebeleci a conexão com a database via moongose

--- crud_app/server.js
+++ crud_app/server.js
@@ -3,6 +3,7 @@ const dotenv = require ('dotenv');
 const morgan = require('morgan');
 const bodyparser =require('body-parser');
 const path = require('path');
+const connectDB = require('./server/database/connection')
 
 const app = express()
 
@@ -12,6 +13,9 @@ const PORT = process.env.PORT || 5000;
 //log requests
 app.use(morgan('tiny'));
 
+
+//mongodb connection;
+connectDB()
 //parse request to body-parser
 app.use(bodyparser.urlencoded({extended:true}))
 
