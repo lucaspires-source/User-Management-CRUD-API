9af4e52cad4a8ec3ff02907a4339cc2c83c03881
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-12 12:52:41 -0300

    podemos procurar um unico usario pela id agora

--- crud_app/server/controller/controller.js
+++ crud_app/server/controller/controller.js
@@ -31,17 +31,30 @@ exports.create = (req, res) => {
 
 //retrieve user
 exports.find = (req, res) => {
-  Userdb.find()
-    .then((user) => {
-      res.send(user);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          errr.message ||
-          "Some error occurred while retrivind user information create operation",
-      });
-    });
+    if(req.query.id){
+        const id = req.query.id;
+
+        Userdb.findById(id)
+            .then(data =>{
+                if(!data){
+                    res.status(404).send({ message : "Not found user with id "+ id})
+                }else{
+                    res.send(data)
+                }
+            })
+            .catch(err =>{
+                res.status(500).send({ message: "Erro retrieving user with id " + id})
+            })
+
+    }else{
+        Userdb.find()
+            .then(user => {
+                res.send(user)
+            })
+            .catch(err => {
+                res.status(500).send({ message : err.message || "Error Occurred while retriving user information" })
+            })
+    }
 };
 
 //update user
