beb00f604bbdd579df05002f3dbe67e03c7eedae
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-12 10:25:12 -0300

    podemos fazer post request na API,agora

--- crud_app/server/controller/controller.js
+++ crud_app/server/controller/controller.js
@@ -2,7 +2,27 @@ const Userdb = require('../model/model')
 
 //create user
 exports.create = (req,res) =>{
+    if(!req.body){
+        res.status(400).send({message:"Content can not be empty!"})
+        return;
+    }
 
+    //new user
+    const user = Userdb({
+        name:req.body.name,
+        email:req.body.email,
+        gender:req.body.gender,
+        status:req.body.status,
+    })
+    // save user in the database
+    user
+        .save(user)
+        .then(data =>{
+            res.send(data)
+        })
+        .catch(err =>{
+            res.status(500).send({message:errr.message || "Some error occurred while creating a create operation"})
+        })
 }
 
 //retrieve user
