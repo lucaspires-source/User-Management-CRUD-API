03781c50fbef94af66a793db3e493db4eda69f48
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-12 14:36:38 -0300

    podemos adicionar clientes pelo lado do client

--- crud_app/server/services/render.js
+++ crud_app/server/services/render.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 exports.homeRoutes = ( req,res) =>{
     axios.get('http://localhost:3000/api/users')
-    .then((response) => {
+    .then((response) =>  {
         res.render('index',{users:response.data})
     })
     .catch(err =>{
@@ -12,10 +12,19 @@ exports.homeRoutes = ( req,res) =>{
     })
 }
 
-exports.update_user = ( req,res) =>{
-    res.render('update_user');
-}
+
 
 exports.add_user = ( req,res) =>{
     res.render('add_user');
+}
+
+exports.update_user = ( req,res) =>{
+    axios.get('http://localhost:3000/api/users',{params:{id : req.query.id}})
+    .then(function(userdata){
+        res.render('update_user',{user:userdata.data});
+    })
+    .catch(err =>{
+        response.send(err)
+    })
+    
 }
